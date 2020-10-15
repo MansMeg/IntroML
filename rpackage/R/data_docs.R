1eab5b8e57661916921fcab47a092192907ec0fa
Måns Magnusson <mons.magnusson@gmail.com>
2020-10-15 22:39:46 +0200

    Init R package

---
+++ rpackage/R/data_docs.R
@@ -0,0 +1,10 @@
+#' The Hitters Dataset
+#'
+#' @description
+#' Major League Baseball Data from the 1986 and 1987 seasons.
+#'
+#' @details
+#' The dataset has been taken from the ISLR R package dataset with
+#' the same name. Please refer to the original dataset for details.
+#'
+"Hitters"
---
+++ rpackage/data-raw/Hitters.R
@@ -0,0 +1,4 @@
+#install.packages("ISLR")
+library(ISLR)
+data("Hitters")
+usethis::use_data(Hitters, overwrite = TRUE)
