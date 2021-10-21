3d18a081fc8e598528ddc23fb6dd8143ae2272f3
Måns Magnusson <mons.magnusson@gmail.com>
2021-10-21 16:04:27 +0200

    bug fix

--- rpackage/data-raw/transformer_example.R
+++ rpackage/data-raw/transformer_example.R
@@ -7,7 +7,7 @@ for(i in seq_along(tokens)){
 }
 word_embeddings <- do.call(rbind, word_embeddings)
 rownames(word_embeddings) <- tokens
-word_embeddings[[7]] <- word_embeddings[[1]]
+word_embeddings[7,] <- word_embeddings[1,]
 
 Wq_mh <- array(rnorm(3*5*4), dim = c(5, 3, 4))/10
 Wk_mh <- array(rnorm(3*5*4), dim = c(5, 3, 4))/10
